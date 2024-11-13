d1b99de3e10f5ca2d4691109121cc9b778b82e15
Mohamed Raslan <mohamedraslan@Mohameds-MacBook-Air.local>
2024-11-13 22:47:12 +0200

    added a singleton firebase services class

---
+++ lib/common_mvc/common_model/firebase_models/firebase_services_singleton.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
+
+class FirebaseService {
+  // Singleton instance
+  //The private constructor _internal is called only once when the instance is created and stored in the static final variable
+  //In a singleton pattern, we want only one instance of a class throughout the app.
+  //By creating a private named constructor _internal(),
+  //we ensure that the instance of the class can only be created within the class itself.
+  static final FirebaseService _instance = FirebaseService._internal();
+
+
+  // Factory constructor to return the same singleton instance every time it's called
+  factory FirebaseService() {
+    return _instance;
+  }
+
+  // Private constructor
+  FirebaseService._internal();
+
+  Future<void> initializeFirebase() async {
+    await Firebase.initializeApp();
+  }
+}
