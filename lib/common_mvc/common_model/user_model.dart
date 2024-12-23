e14f11633ef8651d70cc441711ef7846f8552cd6
rawanabdulhayy <rawanabdulhay29700@gmail.com>
2024-12-23 12:02:46 +0200

    added the setter for the user points

--- lib/common_mvc/common_model/user_model.dart
+++ lib/common_mvc/common_model/user_model.dart
@@ -2,4 +2,9 @@ class User {
   int _points = 0; // Private field to store points
   // Getter for points
   int get points => _points;
+
+  // Private setter for points
+  void _setPoints(int points) {
+    _points = points;
+  }
 }
