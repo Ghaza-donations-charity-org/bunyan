92ea25c8efda21527500df70c8c4e4a0579bbda9
Mohamed Raslan <mohamedraslan@Mohameds-MacBook-Air.local>
2024-12-23 12:32:25 +0200

    added animated transition to navigationController & main screens

--- lib/features/main_screens_wrapper/view/widget/mobile_app_view.dart
+++ lib/features/main_screens_wrapper/view/widget/mobile_app_view.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:ghaza_donations_app/features/main_screens_wrapper/view/list_of_main_screens.dart';
 import 'package:ghaza_donations_app/features/navigation_bar/controller/navigation_controller.dart';
+import 'package:ghaza_donations_app/features/navigation_bar/view/bottom_navigation_bar.dart';
 import 'package:provider/provider.dart';
-
-import '../../../navigation_bar/view/bottom_navigation_bar.dart';
 
 class MobileAppView extends StatelessWidget {
   const MobileAppView({super.key});
@@ -11,11 +10,18 @@ class MobileAppView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Consumer<NavigationController>(
-        builder: (context, navigationController, child) {
-      return Scaffold(
-      body: listOfMainScreens[navigationController.currentIndex],
-      bottomNavigationBar: const BottomNavigationBarWidget(),
+      builder: (context, navigationController, child) {
+        return Scaffold(
+          body: PageView(
+            controller: navigationController.pageController,
+            onPageChanged: (index) {
+              navigationController.updateIndex(index);
+            },
+            children: listOfMainScreens,
+          ),
+          bottomNavigationBar: const BottomNavigationBarWidget(),
+        );
+      },
     );
-    });
   }
 }
