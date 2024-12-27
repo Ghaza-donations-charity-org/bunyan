d473e0a37dcbcadc2203ce1440cad1e55a0314a8
rawanabdulhayy <rawanabdulhay29700@gmail.com>
2024-12-27 13:09:36 +0200

    removed unnecessary code lines

--- lib/common_mvc/common_model/user_model.dart
+++ lib/common_mvc/common_model/user_model.dart
@@ -1,40 +1,3 @@
-// class UserModel {
-//   int _points = 0; // Private field to store points
-//
-//   //todo: this checks current user and manipulates his field (points) instead
-//   // Getter for points
-//   int get points => _points;
-//
-//   // Private setter for points
-//   void _setPoints(int points) {
-//     _points = points;
-//   }
-//
-//   // Example logic to check if the verifier is valid
-//   bool _isAdmin(String verifier) {
-// //TODO: This could be a part of the protection proxy? like this can call a function men henak to check for credentials or validity.
-//     return verifier == "admin_token";
-//   }
-//   // Method to manipulate points, restricted by a verifier
-//   void manipulatePoints(int amount, String verifier) {
-//     if (_isAdmin(verifier)) {
-//       _setPoints(_points + amount);
-//     } else {
-//       throw Exception("Unauthorized access to manipulate points.");
-//     }
-//   }
-//
-//   // Method for automatic updates, restricted by an authorization key
-//   void autoUpdatePoints(int amount, String triggerKey) {
-//     // Replace "autoTriggerKey" with your actual authorization key for external logic
-//     if (triggerKey == "autoTriggerKey") {
-//       _setPoints(_points + amount);
-//     } else {
-//       throw Exception("Unauthorized access to auto-update points.");
-//     }
-//   }
-// }
-
 import 'firebase_models/firebase_facade.dart';
 
 class UserModel {
@@ -84,6 +47,7 @@ class UserModel {
 
   // Check if the verifier is valid
   bool _isAdmin(String verifier) {
+    //TODO: This could be a part of the protection proxy? like this can call a function men henak to check for credentials or validity.
     return verifier == "admin_token";
   }
 
