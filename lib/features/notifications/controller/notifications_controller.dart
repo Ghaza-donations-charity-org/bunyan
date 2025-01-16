5396ededae394d5d9a8b77ba6a12712fe57c7b01
rawanabdulhayy <rawanabdulhay29700@gmail.com>
2025-01-16 14:07:21 +0200

    updated the notification controller file to have the necessary package functions for notifs control

--- lib/features/notifications/controller/notifications_controller.dart
+++ lib/features/notifications/controller/notifications_controller.dart
@@ -1,5 +1,6 @@
 //Notifications controller
 import '../model/observer.dart';
+import 'package:awesome_notifications/awesome_notifications.dart';
 
 class NotificationManager {
   final List<Observer> _observers = [];
@@ -21,5 +22,27 @@ class NotificationManager {
   // Simulate receiving a new notification (for testing)
   void newNotification(String message) {
     notifyObservers(message);
+  }
+}
+
+class NotificationController {
+  @pragma("vm:entry-point")
+  static Future<void> onNotificationCreatedMethod(ReceivedNotification receivedNotification) async {
+    print("Notification created: ${receivedNotification.title}");
+  }
+
+  @pragma("vm:entry-point")
+  static Future<void> onNotificationDisplayedMethod(ReceivedNotification receivedNotification) async {
+    print("Notification displayed: ${receivedNotification.title}");
+  }
+
+  @pragma("vm:entry-point")
+  static Future<void> onDismissActionReceivedMethod(ReceivedNotification receivedNotification) async {
+    print("Notification dismissed: ${receivedNotification.title}");
+  }
+
+  @pragma("vm:entry-point")
+  static Future<void> onActionReceivedMethod(ReceivedNotification receivedNotification) async {
+    print("Notification action clicked and this happens : ${receivedNotification.title}");
   }
 }
