16110e3b9e6211f4a1d84a59ecddb4950b0e3184
rawanabdulhayy <rawanabdulhay29700@gmail.com>
2025-01-16 21:26:08 +0200

    added the event model basic attributes and toggle functions

--- lib/features/events_screen/view/page/events_screen.dart
+++ lib/features/events_screen/view/page/events_screen.dart
@@ -7,7 +7,7 @@ class EventsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Text('Settings screen'),
+        child: Text('Events screen'),
       ),
     );
   }
